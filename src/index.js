ca511d6d7e1f21b1abcd93ab7923cec1a18eeabd
Nick Lemoing <nicklemoign@Nicks-MacBook-Pro.local>
2020-09-16 10:02:38 -0400

    Init npm package

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import editing from "babel-plugins/editing";
+
+import Input from "ui/Input";
+
+const plugins = {
+    editing,
+};
+
+const ui = {
+    Input,
+};
+
+export default {
+    plugins,
+    ui,
+};
